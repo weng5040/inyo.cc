3ddc85ce8815215dfa6aeeb63ac3c5f56e65c5a4
CoolMeWeng <coolme99@qq.com>
2024-11-09 19:41:21 +0000

    1

--- .sh/docker-compose-up.sh
+++ .sh/docker-compose-up.sh
@@ -13,8 +13,8 @@ PROJECTS=(
 for PROJECT_PATH in "${PROJECTS[@]}"; do
     echo "正在更新项目：$PROJECT_PATH"
     cd "$PROJECT_PATH" || continue
-    docker-compose pull
-    docker-compose up -d --remove-orphans
+    #此命令会检查并更新所有服务的镜像，如果有新版本则拉取，并在后台运行最新的容器。`--remove-orphans` 选项确保删除配置文件中已移除的旧容器，以保持环境清洁。
+    docker-compose up -d --remove-orphans --pull always #
 done
 
 echo "所有 docker-compose 项目已更新完成。"
