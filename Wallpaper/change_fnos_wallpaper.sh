0cce78213d54bf123d21245c707ecfffd2636d2c
CoolMeWeng <coolme99@qq.com>
2025-01-08 08:48:45 +0000

    1

--- Wallpaper/change_fnos_wallpaper.sh
+++ Wallpaper/change_fnos_wallpaper.sh
@@ -6,24 +6,19 @@ lang="zh-CN"
 # 如需收集保存壁纸,请去掉下面注释,设置保存文件夹路径
 # 在FileStation里面右键文件夹属性可以看到路径
 # 定义保存路径，如果用户希望收集并保存下载的壁纸。
-savepath="/vol1/BingWallpaper"
+savepath="/vol1/1000/Img/BingWallpaper"
 
-# 如需下载4k分辨率,请设置res=4k
-# 如需下载体积更大的4k以上分辨率的原始图片,请设置res=raw
-# 选择图片的分辨率，可以设置为4K或原始分辨率。
-res=raw
+# 设置FNOS壁纸路径变量
+wallpaper_path="/usr/trim/var/trim_sac/wallpaper/1000/86a28814-a37e-4a16-a2c4-abd001ccc876.jpg"
+
+# 设置缓存壁纸路径变量
+thumb_wallpaper_path="/usr/trim/var/trim_sac/wallpaper/1000/thumb-86a28814-a37e-4a16-a2c4-abd001ccc876.jpg"
 
 # 提示正在收集信息
-echo "[x]Collecting information..."
+echo "[x] 正在收集信息..."
 
 # 定义Bing API的请求链接，获取最新一张图片的信息。
 pic="https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
-
-# 如果选择的分辨率不为空，更新链接以获取4K或更高分辨率的图片。
-if [ "$res" != "" ]
-then
-    pic="https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&uhd=1&uhdwidth=3840&uhdheight=2160"
-fi
 
 # 使用wget获取API的JSON响应，设置语言参数。
 pic=$(wget -t 5 --no-check-certificate -qO- $pic --header="cookie:_EDGE_S=mkt=$lang")
@@ -32,22 +27,12 @@ pic=$(wget -t 5 --no-check-certificate -qO- $pic --header="cookie:_EDGE_S=mkt=$l
 echo $pic | grep -q startdate || exit
 
 # 解析返回结果，提取图片的下载链接。
-link=$(echo https://www.bing.com$(echo $pic | sed 's/.\+"url"[:" ]\+//g' | sed 's/".\+//g'))
+thumb_link=$(echo https://www.bing.com$(echo $pic | sed 's/.\+"url"[:" ]\+//g' | sed 's/".\+//g'))
+link=$(echo $thumb_link | sed 's/1920x1080/UHD/g')
 
-# 如果选择的是原始分辨率，则从解析结果中提取完整的链接。
-if [ "$res" == "raw" ]
-then
-    link=$(echo $link | grep -Eo "https://[-=?/._a-zA-Z0-9]+")
-fi
-
-# 提取图片的发布日期
+# 提取图片的发布日期，如果没有获取到则使用当前日期
 date=$(echo $pic | grep -Eo '"startdate":"[0-9]+' | grep -Eo '[0-9]+' | head -1)
-
-# 如果没有获取到发布日期，则使用当前日期
-if [ "$date" == "" ]
-then
-    date=$(date +%Y%m%d)
-fi
+date=${date:-$(date +%Y%m%d)}
 
 # 提取图片的标题
 title=$(echo $pic | sed 's/.\+"title":"//g' | sed 's/".\+//g')
@@ -58,60 +43,55 @@ copyright=$(echo $pic | sed 's/.\+"copyright[:" ]\+//g' | sed 's/".\+//g')
 # 提取关键词，用于生成文件名
 keyword=$(echo $copyright | sed 's/, /-/g' | cut -d" " -f1 | grep -Eo '[^()\\/:*?"<>]+' | head -1)
 
-# 构建JPG和WebP文件的文件名
+# 构建JPG文件的文件名
 filename="bing_"$date"_"$keyword".jpg"
-webpfile="bing_"$date"_"$keyword".webp"
 
 # 输出获取到的关键信息
-echo "Link:"$link
-echo "Date:"$date
-echo "Title:"$title
-echo "Copyright:"$copyright
-echo "Keyword:"$keyword
-echo "Filename:"$filename
-echo "Webp Filename:"$webpfile
+echo "PIC: $pic"
+echo "链接: $link"
+echo "缩略图链接: $thumb_link"
+echo "发布日期: $date"
+echo "标题: $title"
+echo "版权信息: $copyright"
+echo "关键词: $keyword"
+echo "文件名: $filename"
 
 # 提示正在下载壁纸
-echo "[x]Downloading wallpaper..."
+echo "[x] 正在下载壁纸..."
 
 # 定义临时文件路径并下载图片
 tmpfile=/tmp/$filename
+thumb_tmpfile=/tmp/thumb-$filename
 wget -t 5 --no-check-certificate $link -qO $tmpfile
+wget -t 5 --no-check-certificate $thumb_link -qO $thumb_tmpfile
 
 # 检查下载是否成功
-ls -lah $tmpfile || exit
-
-# 提示正在将JPG转换为WebP格式
-echo "[x]Converting JPG to WebP..."
-
-# 使用ImageMagick的convert命令将JPG格式的壁纸转换为WebP格式
-convert $tmpfile /tmp/$webpfile
-
-# 保存壁纸到指定的路径
-if [ "$savepath" != "" ]
-then
-    echo "[x]Saving wallpaper to savepath..."
-    mkdir -p "$savepath"
-    cp $tmpfile "$savepath/$filename"
-    #cp /tmp/$webpfile "$savepath/$(basename $webpfile)"
-    chmod 755 "$savepath/$filename"
-    #chmod 777 "$savepath/$(basename $webpfile)"
+if [[ ! -f $tmpfile || ! -f $thumb_tmpfile ]]; then
+    echo "[x] 下载失败！"
+    exit 1
 fi
 
-# 提示正在将WebP格式的壁纸复制到fnos路径
-echo "[x]Copying WebP wallpaper to fnos path..."
+# 保存高清壁纸到指定的路径
+if [ "$savepath" != "" ]; then
+    echo "[x] 正在保存高清壁纸到保存路径..."
+    mkdir -p "$savepath"
+    cp $tmpfile "$savepath/$filename"
+    chmod 755 "$savepath/$filename"
+fi
 
-# 将转换后的WebP文件复制到指定路径
-cp -f /tmp/$webpfile /usr/trim/www/static/bg/wallpaper-1.webp
+# 提示正在将图片复制到FNOS路径
+echo "[x] 正在复制JPG壁纸到FNOS路径..."
 
-# 更改新壁纸的权限为可读写
-chmod 777 /usr/trim/www/static/bg/wallpaper-1.webp
+# 使用 wallpaper_path 变量复制高清壁纸
+sudo cp -f /tmp/$filename "$wallpaper_path"
+sudo cp -f $thumb_tmpfile "$thumb_wallpaper_path"
 
 # 提示正在清理临时文件
-echo "[x]Clean..."
+echo "[x] 清理临时文件..."
 
 # 删除下载的临时文件
-rm -f /tmp/bing_*.jpg /tmp/bing_*.webp
+sudo rm -f /tmp/bing_*.jpg
+sudo rm -f /tmp/thumb-bing_*.jpg
 
 # 提示操作完成
-echo "[x]Done. Your wallpaper is updated."
+echo "[x] 完成！你的壁纸已更新。"
